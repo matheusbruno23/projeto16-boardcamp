20ec07ce834df166f77ca51c740c778c7e760ec2
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-05-19 11:27:18 -0300

    projeto finalizado, sem bonus

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -2,26 +2,71 @@ import { db } from "../database/database.js"
 import dayjs from "dayjs"
 
 export async function getRentals(req, res) {
-    try {
 
-        res.sendStatus(200)
+    try {
+        const {rentals} = await db.query(`
+        SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName" 
+        FROM rentals
+        JOIN customers ON rentals."customerId" = customers.id
+        JOIN games ON rentals."gameId" = games.id;
+        `)
+
+        const resultRentals = rentals.map((r) => {
+            const rentalResponse = {
+                ...r,
+                customer:{
+                    id: r.customerId,
+                    name: r.customerName
+                },
+                game:{
+                    id: r.gameId, 
+                    name: r.gameName 
+                }
+            }
+
+            delete rentalResponse.customerName
+            delete rentalResponse.gameName
+            return rentalResponse 
+        })
+
+        res.send(resultRentals)
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
 export async function createRental(req, res) {
+    const { customerId, gameId, daysRented } = req.body
+    const { pricePerDay } = res.locals
 
     try {
-        res.sendStatus(200)
+        await db.query(`
+        INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee")
+            VALUES ($1, $2, $3, $4, $5, null, null);
+       `, [customerId, gameId, daysRented, dayjs().format('YYYY-MM-DD'), pricePerDay * daysRented])
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
 export async function finishRental(req, res) {
+    const { id } = req.params
+    const { pricePerDay, daysRented, rentDate } = res.locals
+    let delayFee = null
+
+    const difference = dayjs().diff(dayjs(rentDate), 'days')
+
+    if (difference > daysRented) {
+        delayFee = pricePerDay * (difference - daysRented)
+    }
 
     try {
+        await db.query(`
+            UPDATE rentals
+                SET "returnDate"=$1, "delayFee"=$2
+                WHERE id=$3;
+        `, [dayjs().format('YYYY-MM-DD'), delayFee, id])
         res.sendStatus(200)
 
     } catch (err) {
@@ -30,8 +75,9 @@ export async function finishRental(req, res) {
 }
 
 export async function deleteRental(req, res) {
-
+    const { id } = req.params
     try {
+        await db.query(`DELETE FROM rentals WHERE id=$1`, [id])
         res.sendStatus(200)
     } catch (err) {
         res.status(500).send(err.message)
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express"
 import { createRental, deleteRental, getRentals, finishRental } from "../controllers/rentals.controllers.js"
 import validateSchema from "../middlewares/validateSchema.middleware.js"
 import { rentalSchema } from "../schemas/rentals.schemas.js"
-
+import { validateCreateRental , validateDeleteRental , validateReturnRental } from "../middlewares/rentals.middleware.js"
 const rentalsRouter = Router()
 
 rentalsRouter.get("/rentals", getRentals)
-rentalsRouter.post("/rentals", validateSchema(rentalSchema), createRental)
-rentalsRouter.post("/rentals/:id/return", finishRental)
-rentalsRouter.delete("/rentals/:id", deleteRental)
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), validateCreateRental , createRental)
+rentalsRouter.post("/rentals/:id/return", validateReturnRental, finishRental)
+rentalsRouter.delete("/rentals/:id", validateDeleteRental,deleteRental)
 
 export default rentalsRouter
